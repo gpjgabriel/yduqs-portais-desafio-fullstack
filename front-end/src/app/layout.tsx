b4d1fe065dcbb862fdebc45f1b0f70a1f1468d4f
Gabriel Paiva Justo <gpj_gabriel@hotmail.com>
2025-11-10 19:42:04 -0300

    start(libs): start project - install libs and dependencies

---
+++ front-end/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "primereact/resources/themes/saga-blue/theme.css"; 
+import "primereact/resources/primereact.min.css";         
+import "primeicons/primeicons.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Teste YDUQS",
+  description: "Teste Técnico YDUQS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${inter} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
