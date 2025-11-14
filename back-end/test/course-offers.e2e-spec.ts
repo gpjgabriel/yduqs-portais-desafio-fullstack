d1fea7127a6b6042d02062017401406766e3d899
Gabriel Paiva Justo <gpj_gabriel@hotmail.com>
2025-11-14 08:01:40 -0300

    feat(courseOffer): test GET route - simple test

--- back-end/test/course-offers.e2e-spec.ts
+++ back-end/test/course-offers.e2e-spec.ts
@@ -1,25 +1,29 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
-import { AppModule } from "./../src/app.module";
+import { AppModule } from "../src/app.module";
+import { PrismaService } from "prisma/prisma.service";
 
-describe("AppController (e2e)", () => {
+describe("CourseOffers (e2e)", () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    prisma = app.get<PrismaService>(PrismaService);
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it("/Rota básica para teste - course-offers (GET)", () => {
+  it("GET /course-offers -> deve retornar um array vazio", () => {
     return request(app.getHttpServer())
       .get("/course-offers")
       .expect(200)
